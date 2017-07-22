4da684ddc44d90f0b4bd2b0c3c86a36b7495e8bb
Galen Gidman <galen@galengidman.com>
2017-07-22 11:30:56 -0500

    :hammer: better ensure that taxonomy slugs don't use reserved terms

--- inc/admin.php
+++ inc/admin.php
@@ -63,7 +63,7 @@ function helppress_admin_panel() {
 		'name'               => esc_html__( 'Category URL slug', 'helppress' ),
 		'desc'               => sprintf( __( 'The URL slug for knowledge base categories. <code>%s</code>', 'helppress' ), trailingslashit( home_url() ) . '<ins>' . helppress_get_option( 'category_slug' ) . '</ins>/category-title/' ),
 		'default'            => helppress_get_option_default( 'category_slug' ),
-		'sanitize_callbacks' => array( 'sanitize_title', 'helppress_ensure_unique_category_slug' ),
+		'sanitize_callbacks' => array( 'sanitize_title', 'helppress_ensure_safe_tax_slug' ),
 	) );
 
 	// Tag URL slug
@@ -73,7 +73,7 @@ function helppress_admin_panel() {
 		'name'               => esc_html__( 'Tag URL slug', 'helppress' ),
 		'desc'               => sprintf( __( 'The URL slug for knowledge base tags. <code>%s</code>', 'helppress' ), trailingslashit( home_url() ) . '<ins>' . helppress_get_option( 'tag_slug' ) . '</ins>/tag-title/' ),
 		'default'            => helppress_get_option_default( 'tag_slug' ),
-		'sanitize_callbacks' => array( 'sanitize_title', 'helppress_ensure_unique_tag_slug' ),
+		'sanitize_callbacks' => array( 'sanitize_title', 'helppress_ensure_safe_tax_slug' ),
 	) );
 
 	// Enable tags
@@ -163,25 +163,98 @@ function helppress_admin_panel() {
 
 }
 add_action( 'tf_create_options', 'helppress_admin_panel' );
-
 add_action( 'tf_save_admin_helppress', 'flush_rewrite_rules' );
 
-function helppress_ensure_unique_category_slug( $slug ) {
+function helppress_ensure_safe_tax_slug( $slug ) {
 
-	// "category" is already in use by WordPress core
-	if ( $slug === 'category' ) {
-		$slug = 'kb-category';
-	}
+	// https://codex.wordpress.org/Reserved_Terms
+	$reserved_terms = array(
+		'attachment',
+		'attachment_id',
+		'author',
+		'author_name',
+		'calendar',
+		'cat',
+		'category',
+		'category__and',
+		'category__in',
+		'category__not_in',
+		'category_name',
+		'comments_per_page',
+		'comments_popup',
+		'custom',
+		'customize_messenger_channel',
+		'customized',
+		'cpage',
+		'day',
+		'debug',
+		'embed',
+		'error',
+		'exact',
+		'feed',
+		'hour',
+		'link_category',
+		'm',
+		'minute',
+		'monthnum',
+		'more',
+		'name',
+		'nav_menu',
+		'nonce',
+		'nopaging',
+		'offset',
+		'order',
+		'orderby',
+		'p',
+		'page',
+		'page_id',
+		'paged',
+		'pagename',
+		'pb',
+		'perm',
+		'post',
+		'post__in',
+		'post__not_in',
+		'post_format',
+		'post_mime_type',
+		'post_status',
+		'post_tag',
+		'post_type',
+		'posts',
+		'posts_per_archive_page',
+		'posts_per_page',
+		'preview',
+		'robots',
+		's',
+		'search',
+		'second',
+		'sentence',
+		'showposts',
+		'static',
+		'subpost',
+		'subpost_id',
+		'tag',
+		'tag__and',
+		'tag__in',
+		'tag__not_in',
+		'tag_id',
+		'tag_slug__and',
+		'tag_slug__in',
+		'taxonomy',
+		'tb',
+		'term',
+		'terms',
+		'theme',
+		'title',
+		'type',
+		'w',
+		'withcomments',
+		'withoutcomments',
+		'year',
+	);
 
-	return $slug;
-
-}
-
-function helppress_ensure_unique_tag_slug( $slug ) {
-
-	// "tag" is already in use by WordPress core
-	if ( $slug === 'tag' ) {
-		$slug = 'kb-tag';
+	if ( in_array( $slug, $reserved_terms ) ) {
+		$slug = 'kb-' . $slug;
 	}
 
 	return $slug;
