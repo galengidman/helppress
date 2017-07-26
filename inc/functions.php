0e26f702c128e339a827fbf6e0e1fc5d06488ceb
Galen Gidman <galen@galengidman.com>
2017-07-25 20:15:55 -0500

    :art: alphabetically re-order functions.php file

--- inc/functions.php
+++ inc/functions.php
@@ -2,18 +2,19 @@
 
 if ( ! defined( 'ABSPATH' ) ) exit;
 
-function helppress_get_categories( $args = array() ) {
+function helppress_article_format_icon( $post_id, $size = 16 ) {
 
-	$args = wp_parse_args( $args, array(
-		'taxonomy' => 'helppress_article_category',
-		'orderby'  => 'menu_order',
-	) );
+	$format = helppress_get_post_format( $post_id );
 
-	$args = apply_filters( 'helppress_get_categories_args', $args );
+	return helppress_genericon( $format, $size );
 
-	$terms = get_terms( $args );
+}
 
-	return apply_filters( 'helppress_get_categories', $terms );
+function helppress_genericon( $icon, $size = 16 ) {
+
+	$svg_url = esc_url( HELPPRESS_URL . '/assets/img/genericons-neue.svg' );
+
+	return "<svg class='genericons-neue genericons-neue-{$icon}' width='{$size}px' height='{$size}px'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='{$svg_url}#{$icon}'></use></svg>";
 
 }
 
@@ -32,26 +33,26 @@ function helppress_get_articles( $args = array() ) {
 
 }
 
+function helppress_get_categories( $args = array() ) {
+
+	$args = wp_parse_args( $args, array(
+		'taxonomy' => 'helppress_article_category',
+		'orderby'  => 'menu_order',
+	) );
+
+	$args = apply_filters( 'helppress_get_categories_args', $args );
+
+	$terms = get_terms( $args );
+
+	return apply_filters( 'helppress_get_categories', $terms );
+
+}
+
 function helppress_get_post_format( $post_id ) {
 
 	$format = get_post_format( $post_id );
+	$format = $format ? $format : 'standard';
 
-	return $format ? $format : 'standard';
-
-}
-
-function helppress_article_format_icon( $post_id, $size = 16 ) {
-
-	$format = helppress_get_post_format( $post_id );
-
-	return helppress_genericon( $format, $size );
-
-}
-
-function helppress_genericon( $icon, $size = 16 ) {
-
-	$svg_url = esc_url( HELPPRESS_URL . '/assets/img/genericons-neue.svg' );
-
-	return "<svg class='genericons-neue genericons-neue-{$icon}' width='{$size}px' height='{$size}px'><use xmlns:xlink='http://www.w3.org/1999/xlink' xlink:href='{$svg_url}#{$icon}'></use></svg>";
+	return apply_filters( 'helppress_get_post_format', $format );
 
 }
