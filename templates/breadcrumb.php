c3e57e866f7c04e5304981eb645d2c4e3cd86327
Galen Gidman <galen@galengidman.com>
2017-07-26 00:58:40 -0500

    :sparkles: microdata and better classes for breadcrumb markup

--- templates/breadcrumb.php
+++ templates/breadcrumb.php
@@ -1,9 +1,10 @@
-<ol class="helppress__breadcrumb">
-	<?php foreach ( helppress_get_breadcrumb() as $crumb ) : ?>
-		<li>
-			<a href="<?php echo esc_url( $crumb['url'] ); ?>">
-				<span><?php echo $crumb['label']; ?></span>
+<ol class="helppress__breadcrumb" itemscope itemtype="http://schema.org/BreadcrumbList">
+	<?php $i = 1; foreach ( helppress_get_breadcrumb() as $crumb ) : ?>
+		<li class="helppress__breadcrumb-crumb" itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+			<a href="<?php echo esc_url( $crumb['url'] ); ?>" class="helppress__breadcrumb-link" itemprop="item">
+				<span itemprop="name"><?php echo $crumb['label']; ?></span>
+				<meta itemprop="position" content="<?php echo esc_attr( $i ); ?>">
 			</a>
 		</li>
-	<?php endforeach; ?>
+	<?php $i++; endforeach; ?>
 </ol>
