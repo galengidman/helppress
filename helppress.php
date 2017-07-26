bd0fbae2cd7c81639899fd71119bd6f42c02a09f
Galen Gidman <galen@galengidman.com>
2017-07-25 23:40:09 -0500

    :hammer: update main plugin file to class

--- helppress.php
+++ helppress.php
@@ -3,20 +3,65 @@
  * Plugin Name: HelpPress
  */
 
-if ( ! defined( 'HELPPRESS_PATH' ) ) define( 'HELPPRESS_PATH', untrailingslashit( plugin_dir_path( __FILE__ ) ) );
-if ( ! defined( 'HELPPRESS_URL' ) )  define( 'HELPPRESS_URL',  untrailingslashit( plugin_dir_url( __FILE__ ) ) );
+if ( ! class_exists( 'HelpPress' ) ) {
 
-include HELPPRESS_PATH . '/inc/vendor/gamajo/template-loader/class-gamajo-template-loader.php';
-include HELPPRESS_PATH . '/inc/vendor/gambitph/titan-framework/titan-framework.php';
+	class HelpPress {
 
-include HELPPRESS_PATH . '/inc/classes/class-helppress-template-loader.php';
-include HELPPRESS_PATH . '/inc/classes/class-helppress-search-autocomplete.php';
-include HELPPRESS_PATH . '/inc/classes/class-helppress-menu-archive-link.php';
-include HELPPRESS_PATH . '/inc/classes/class-helppress-post-types.php';
-include HELPPRESS_PATH . '/inc/classes/class-helppress-taxonomies.php';
-include HELPPRESS_PATH . '/inc/classes/class-helppress-templates.php';
+		public function __construct() {
 
-include HELPPRESS_PATH . '/inc/admin.php';
-include HELPPRESS_PATH . '/inc/assets.php';
-include HELPPRESS_PATH . '/inc/functions.php';
-include HELPPRESS_PATH . '/inc/options.php';
+			add_action( 'plugins_loaded', array( $this, 'defineConstants' ) );
+			add_action( 'plugins_loaded', array( $this, 'includes' ) );
+
+		}
+
+		public function defineConstants() {
+
+			$constants = array(
+				'HELPPRESS_PATH' => untrailingslashit( plugin_dir_path( __FILE__ ) ),
+				'HELPPRESS_URL'  => untrailingslashit( plugin_dir_url( __FILE__ ) ),
+			);
+
+			foreach ( $constants as $constant => $value ) {
+				if ( ! defined( $constant ) ) {
+					define( $constant, $value );
+				}
+			}
+
+		}
+
+		public function includes() {
+
+			$includes = array(
+
+				// Vendor
+				'/inc/vendor/gamajo/template-loader/class-gamajo-template-loader.php',
+				'/inc/vendor/gambitph/titan-framework/titan-framework.php',
+
+				// Classes
+				'/inc/classes/class-helppress-breadcrumb.php',
+				'/inc/classes/class-helppress-menu-archive-link.php',
+				'/inc/classes/class-helppress-post-types.php',
+				'/inc/classes/class-helppress-search-autocomplete.php',
+				'/inc/classes/class-helppress-taxonomies.php',
+				'/inc/classes/class-helppress-template-loader.php',
+				'/inc/classes/class-helppress-templates.php',
+
+				// Other
+				'/inc/admin.php',
+				'/inc/assets.php',
+				'/inc/functions.php',
+				'/inc/options.php',
+
+			);
+
+			foreach ( $includes as $file ) {
+				include HELPPRESS_PATH . $file;
+			}
+
+		}
+
+	}
+
+}
+
+new HelpPress();
---
+++ inc/classes/class-helppress-breadcrumb.php
@@ -0,0 +1,15 @@
+<?php
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+if ( ! class_exists( 'HelpPress_Breadcrumb' ) ) {
+
+	class HelpPress_Breadcrumb {
+
+		public function __construct() {
+
+		}
+
+	}
+
+}
