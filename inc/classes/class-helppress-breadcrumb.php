78655525c567e9f6c1e49be7663eb691125636ae
Galen Gidman <galen@galengidman.com>
2017-07-27 22:22:11 -0500

    :hammer: refactor breadcrumb class

--- inc/classes/class-helppress-breadcrumb.php
+++ inc/classes/class-helppress-breadcrumb.php
@@ -6,50 +6,63 @@ if ( ! class_exists( 'HelpPress_Breadcrumb' ) ) {
 
 	class HelpPress_Breadcrumb {
 
-		public function get_trail() {
+		protected $trail = array();
 
-			$trail = array();
+		public function __construct() {
 
-			$trail[] = array(
-				'label' => esc_html__( 'Home', 'helppress' ),
-				'url'   => home_url( '/' ),
+			$this->add(
+				esc_html__( 'Home', 'helppress' ),
+				home_url( '/' )
 			);
 
-			$trail[] = array(
-				'label' => esc_html__( 'Knowledge Base', 'helppress' ),
-				'url'   => helppress_get_knowledge_base_url(),
+			$this->add(
+				esc_html__( 'Knowledge Base', 'helppress' ),
+				helppress_get_knowledge_base_url()
 			);
 
 			if ( is_singular( 'helppress_article' ) ) {
-				$article_categories = wp_get_post_terms( get_the_id(), 'helppress_article_category', array( 'fields' => 'ids' ) );
-				if ( $article_categories ) {
-					$trail = array_merge( $trail, $this->build_tax_tree( $article_categories[0] ) );
+				$article_categories = wp_get_post_terms(
+					get_the_id(),
+					'helppress_article_category',
+					array( 'fields' => 'ids' )
+				);
+
+				if ( $article_categories[0] ) {
+					$this->add_tax_tree( $article_categories[0] );
 				}
 
-				$trail[] = array(
-					'label' => get_the_title(),
-					'url'   => get_permalink(),
+				$this->add(
+					get_the_title(),
+					get_permalink()
 				);
 			}
 
 			if ( is_tax( array( 'helppress_article_category', 'helppress_article_tag' ) ) ) {
-				$trail = array_merge( $trail, $this->build_tax_tree( get_queried_object_id() ) );
+				$this->add_tax_tree( get_queried_object_id() );
 			}
 
 			elseif ( is_search() ) {
-				$trail[] = array(
-					'label' => get_search_query(),
-					'url'   => get_search_link(),
+				$this->add(
+					get_search_query(),
+					get_search_link()
 				);
 			}
 
-			return $trail;
+		}
+
+		public function get_trail() {
+
+			return $this->trail;
 
 		}
 
-		public function build_tax_tree( $term_id = null ) {
+		protected function add( $label, $url ) {
 
-			$trail = [];
+			$this->trail[] = array( 'label' => $label, 'url' => $url );
+
+		}
+
+		protected function add_tax_tree( $term_id ) {
 
 			if ( term_exists( $term_id ) ) {
 
@@ -59,20 +72,18 @@ if ( ! class_exists( 'HelpPress_Breadcrumb' ) ) {
 
 				if ( $ancestors ) {
 					$tree = array_merge( $ancestors, $tree );
+				}
 
-					foreach ( $tree as $term_id ) {
-						$term = get_term( $term_id );
+				foreach ( $tree as $term_id ) {
+					$term = get_term( $term_id );
 
-						$trail[] = array(
-							'label' => $term->name,
-							'url'   => get_term_link( $term ),
-						);
-					}
+					$this->add(
+						$term->name,
+						get_term_link( $term )
+					);
 				}
 
 			}
-
-			return $trail;
 
 		}
 
