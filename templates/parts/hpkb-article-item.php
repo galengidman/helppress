d061e2d4de4d6e2921efd6c0e109f7f5b0668ad9
Galen Gidman <galen@galengidman.com>
2017-08-25 23:43:12 -0500

    :bug: echo permalink

--- templates/parts/hpkb-article-item.php
+++ templates/parts/hpkb-article-item.php
@@ -1,7 +1,7 @@
 <div class="hpkb-article-item">
 	<header class="hpkb-article-item__header">
 		<h4 class="hpkb-article-item__title">
-			<a href="<?php esc_url( get_permalink() ); ?>">
+			<a href="<?php echo esc_url( get_permalink() ); ?>">
 				<?php echo hpkb_article_format_genericon(); ?>
 				<span><?php the_title(); ?></span>
 			</a>
