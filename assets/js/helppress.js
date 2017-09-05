bc5394717ed53b60a70943c773745812b10e4f80
Galen Gidman <galen@galengidman.com>
2017-09-05 18:46:15 -0500

    :sparkles: redirect to article when selecting search suggestion - closes #5

--- assets/js/helppress.js
+++ assets/js/helppress.js
@@ -2,16 +2,19 @@
 
 	$(function() {
 
-		var $searchInput       = $('.helppress-search__input--suggest');
+		var $searchInput = $('.helppress-search__input--suggest');
 		var $searchSuggestions = $('.helppress-search__suggestions');
 
 		$searchInput.devbridgeAutocomplete({
-			serviceUrl:       helppress_l10n.admin_ajax,
-			params:           {
+			serviceUrl: helppress_l10n.admin_ajax,
+			params: {
 				action: 'helppress_autocomplete_suggestions',
 			},
-			appendTo:         $searchSuggestions,
+			appendTo: $searchSuggestions,
 			forceFixPosition: true,
+			onSelect: function(suggestion) {
+				window.location.href = suggestion.data;
+			},
 		});
 
 	});
