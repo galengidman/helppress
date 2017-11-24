0551ffa92f93143160ab8229fbeb94f330b3f81f
Galen Gidman <galen@galengidman.com>
2017-11-24 15:39:32 -0600

    :art: vertical alignment

--- includes/assets.php
+++ includes/assets.php
@@ -38,7 +38,7 @@ function helppress_assets() {
 		'admin_ajax' => esc_url( admin_url( 'admin-ajax.php' ) ),
 	) );
 
-	$disable_css = apply_filters( 'helppress_disable_css', false );
+	$disable_css        = apply_filters( 'helppress_disable_css', false );
 	$disable_css_option = helppress_get_option( 'disable_css' );
 
 	if ( ! $disable_css && ! $disable_css_option ) {
