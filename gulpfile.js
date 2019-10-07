856839b91f88c9191ff94e6a1e800da607a05a35
Galen Gidman <galen@galengidman.com>
2019-10-07 00:18:33 -0500

    gulp redux

--- gulpfile.js
+++ gulpfile.js
@@ -1,75 +1,45 @@
-var autoprefixer  = require('autoprefixer');
-var cssnano       = require('cssnano');
-var del           = require('del');
-var fs            = require('fs');
-var gulp          = require('gulp');
-var perfectionist = require('perfectionist');
-var postcss       = require('gulp-postcss');
-var rename        = require('gulp-rename');
-var sass          = require('gulp-sass');
-var shell         = require('gulp-shell');
-var uglify        = require('gulp-uglify');
-var zip           = require('gulp-zip');
-
-var pkg = JSON.parse(fs.readFileSync('package.json'));
+var gulp = require('gulp');
+var rename = require('gulp-rename');
+var pkg = JSON.parse(require('fs').readFileSync('package.json'));
 
 // DEV -------------------------------------------------------------------------
 
-gulp.task('scss', function() {
+function css() {
+	var perfectionist = require('perfectionist');
+	var postcss = require('gulp-postcss');
+
 	return gulp.src('assets/scss/*.scss')
-		.pipe(sass().on('error', sass.logError))
+		.pipe(require('gulp-sass')().on('error', sass.logError))
 		.pipe(postcss([
-			autoprefixer(),
+			require('autoprefixer')(),
 			perfectionist(),
 		]))
 		.pipe(gulp.dest('assets/dist'))
 		.pipe(postcss([
-			cssnano(),
+			require('cssnano')(),
 			perfectionist({ format: 'compressed' })
 		]))
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest('assets/dist'));
-});
+}
 
-gulp.task('js', function() {
+function js() {
 	return gulp.src('assets/js/*.js')
 		.pipe(gulp.dest('assets/dist'))
-		.pipe(uglify())
+		.pipe(require('gulp-uglify')())
 		.pipe(rename({ suffix: '.min' }))
 		.pipe(gulp.dest('assets/dist'));
-});
+}
 
-gulp.task('dev', function() {
-	gulp.watch('assets/scss/**/*.scss', ['scss']);
-	gulp.watch('assets/scsjss/**/*.js', ['js']);
-});
+function watch() {
+	gulp.watch('assets/scss/**/*.scss', css);
+	gulp.watch('assets/js/**/*.js', js);
+}
 
-gulp.task('default', ['dev']);
+exports.css = css;
+exports.js = js;
+exports.default = watch;
 
 // RELEASE ---------------------------------------------------------------------
 
-gulp.task('copy', function() {
-	return gulp.src([
-			'**',
-			'!composer.json',
-			'!composer.lock',
-			'!gulpfile.js',
-			'!includes/vendor/autoload.php',
-			'!includes/vendor/composer/',
-			'!includes/vendor/composer/**',
-			'!node_modules/',
-			'!node_modules/**',
-			'!package.json',
-		])
-		.pipe(gulp.dest(`${pkg.name}`));
-});
-
-gulp.task('zip', ['copy'], shell.task(`zip -r ${pkg.name}-${pkg.version}.zip ${pkg.name}`));
-
-gulp.task('export', ['zip'], shell.task(`mv ${pkg.name}-${pkg.version}.zip ~/Desktop/`));
-
-gulp.task('clean', ['export'], function() {
-	return del(`${pkg.name}`);
-});
-
-gulp.task('release', ['copy', 'zip', 'export', 'clean']);
+gulp.task('release', require('gulp-shell').task(`zip -r ${pkg.name}-${pkg.version}.zip ${pkg.name} assets/* includes/* templates/* *.php *.txt && mv ${pkg.name}-${pkg.version}.zip ~/Desktop`))
